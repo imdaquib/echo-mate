83ebc1daf7732a7bcb76e6af2ee0a0a1a2328cf5
imdaquib <mdaquib5656@gmail.com>
2023-10-12 16:02:41 +0530

    updated

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const app = express();
 const connectDB = require('./config/db')
 const path = require('path');
+const cors = require('cors')
+const PORT = process.env.PORT || 5000;
 
 /// connect database
 connectDB();
 
 // Init Middleware
+const app = express();
+
 app.use(express.json({extended: false}));
 
 // app.get('/', (req, res) => res.send('Api running'));
@@ -18,16 +21,27 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 // server static assets in production
-if(process.env.NODE_ENV === 'production'){
-      // set static folder
-      app.use(express.static('client/build'));
+// if(process.env.NODE_ENV === 'production'){
+//       // set static folder
+//       app.use(express.static('client/build'));
 
-      app.get('*', (req, res) =>{
-            res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
+//       app.get('*', (req, res) =>{
+//             res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
+//       })
+// }
+
+app.use(express.json());
+
+const _dirname=path.dirname("")
+const buildpath = path.join(_dirname,"client/build")
+app.use(express.static(buildpath));
+
+app.use(
+      cors({
+        "origin": "*",
       })
-}
+    );
 
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
       console.log(`Server is running on port ${PORT}`);
